36782235eafdf5e37ffc977874c43e57fbe4ec12
Zitsu <sunnymavale@gmail.com>
2026-09-17 08:14:04 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Copy the web app config from Firebase Console → Project settings → Your apps.
 // Never place a service-account key in this file.
 window.FOODIE_FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_DATABASE.REGION.firebasedatabase.app",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAAPNK7PEPK90SMSQDCSne3wBr1OYoAXxM",
+  authDomain: "store-data-for-sms.firebaseapp.com",
+  databaseURL: "https://store-data-for-sms-default-rtdb.firebaseio.com",
+  projectId: "store-data-for-sms",
+  storageBucket: "store-data-for-sms.firebasestorage.app",
+  messagingSenderId: "508840668923",
+  appId: "1:508840668923:web:044d77d9bfaa337574ca5e"
 };
